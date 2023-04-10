d89d1130019b810ce11d17948a6e6562afa98766
tomasbroda11 <115743788+tomasbroda11@users.noreply.github.com>
2023-04-10 17:56:28 -0300

    dasdasdasd

---
+++ LabSintaxis3-BBC/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace LabSintaxis3_BBC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cantIteraciones = 5;
+            string[] arrayStrings = new string[cantIteraciones];
+
+            Console.WriteLine("Ingrese 5 palabras");
+            for (int i = 0; i < cantIteraciones; i++)
+            {
+                string palabra = Console.ReadLine();
+                arrayStrings.SetValue(palabra, i);
+            }
+
+            Console.Clear();
+            for (int j = cantIteraciones - 1; j >= 0; j--)
+            {
+                Console.WriteLine(arrayStrings[j]);
+            }
+        }
+    }
+}

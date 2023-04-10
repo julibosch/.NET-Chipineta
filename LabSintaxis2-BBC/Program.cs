5b8f4de8780d2c7f4c56c28f6dbd3118f437a677
tomasbroda11 <115743788+tomasbroda11@users.noreply.github.com>
2023-04-10 17:41:35 -0300

    Lab2

---
+++ LabSintaxis2-BBC/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LabSintaxis2Ej1yEj2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese un texto: ");
+            string inputTexto = Console.ReadLine();
+            if (!inputTexto.Trim().Equals(""))
+            {
+                Console.Write("1-mayus \n2-minus \n3-cant caracteres \nElija opcion: ");
+                ConsoleKeyInfo opc = Console.ReadKey();
+                Console.Clear();
+                switch (opc.Key)
+                {
+                    case ConsoleKey.D1:
+                        {
+                            string mayus = inputTexto.ToUpper();
+                            Console.WriteLine(mayus);
+                            break;
+                        }
+                    case ConsoleKey.D2:
+                        {
+                            string minus = inputTexto.ToLower();
+                            Console.WriteLine(minus);
+                            break;
+                        }
+                    case ConsoleKey.D3:
+                        {
+                            int cantLetras = inputTexto.Length;
+                            Console.WriteLine(cantLetras);
+                            break;
+                        }
+                    default:
+                        break;
+                }
+
+            } else
+            {
+                Console.WriteLine("No ingresaste nada pa");
+                Console.ReadKey();
+            }
+              
+        }     
+    }
+}

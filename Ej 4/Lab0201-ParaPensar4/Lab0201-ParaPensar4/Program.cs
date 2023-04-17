4b9b3b06e7d3172d096004469d56407e9832ebb3
JuliBosch <72669461+julibosch@users.noreply.github.com>
2023-04-17 18:51:03 -0200

    Ej4 y 5

---
+++ Ej 4/Lab0201-ParaPensar4/Lab0201-ParaPensar4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lab0201_ParaPensar4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i%2==0) 
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}

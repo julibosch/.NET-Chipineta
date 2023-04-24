1f155425bac99296889ae89b33612f00a28eed66
JuliBosch <72669461+julibosch@users.noreply.github.com>
2023-04-24 17:58:45 -0300

    LabClases

---
+++ LabClases1/Clases/A.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Clases
+{
+    public class A
+    {
+        public string NombreInstancia { get; set; }
+        public A() { NombreInstancia = "Instancia sin nombre"; }
+        public A(string nombre) { NombreInstancia = nombre; }
+
+        public string MostrarNombre() {
+            return NombreInstancia;
+        }
+
+        public void M1()
+        {
+            Console.WriteLine("METODO M1");
+        }
+
+        public void M2()
+        {
+            Console.WriteLine("METODO M2");
+        }
+
+        public void M3()
+        {
+            Console.WriteLine("METODO M3");
+        }
+    }
+}

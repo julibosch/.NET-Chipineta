8ac27f19d14112abea1af762884babb96d3d3b8e
chipcasla <103225088+chipcasla@users.noreply.github.com>
2023-05-08 17:21:03 -0300

    Geometria

---
+++ Geometria/Circulo.cs
@@ -0,0 +1,26 @@
+﻿namespace Geometria
+{
+    public class Circulo
+    {
+        private int m_radio;
+
+        public int Radio {
+            get {
+                return m_radio;
+            }
+            set {
+                m_radio = value;
+            }
+        }
+
+        public void CalcularPerimetro()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void CalcularSuperficie()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}

4f7dbdf666554c64b55ad11ce4a624d67602f57f
chipcasla <103225088+chipcasla@users.noreply.github.com>
2023-05-15 17:57:26 -0300

    4) Crear Persona

---
+++ Persona/Persona.cs
@@ -0,0 +1,68 @@
+﻿namespace CrearPersona
+{
+    public class Persona
+    {
+        private string nombre;
+        private string apellido;
+        private int dni;
+        private int edad;
+
+        public Persona()
+        {         
+        }
+
+        public Persona(string nombre,string apellido,int edad,int dni)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Edad = edad;
+            Dni = dni;
+
+            Console.WriteLine("Se ha creado con éxito la Persona!");
+
+        }
+
+        ~Persona()
+        {
+            Console.WriteLine("La persona ha sido destruida con éxito!");
+        }
+
+        public string Nombre {
+            get => nombre;
+            set {
+                nombre = value;
+            }
+        }
+
+        public string Apellido {
+            get => apellido;
+            set {
+                apellido = value;   
+            }
+        }
+
+        public int Edad {
+            get => edad;
+            set {
+                edad = value;
+            }
+        }
+
+        public int Dni {
+            get => dni;
+            set {
+                dni = value;    
+            }
+        }
+
+        public string GetFullName()
+        {
+            return this.Nombre + " " + this.Apellido;
+        }
+
+        public int GetAge()
+        {
+            return this.Edad;
+        }
+    }
+}
---
+++ pruebaPersona/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CrearPersona;
+
+
+namespace pruebaPersona // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona p = new Persona("Andres", "Vombergar", 21, 233435353);
+
+            Console.WriteLine(p.GetFullName());
+            Console.WriteLine(p.GetAge());
+            Console.WriteLine();
+        }
+    }
+}
+
+
+

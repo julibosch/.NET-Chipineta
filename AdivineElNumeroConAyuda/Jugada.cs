1e28d3debdf50ed3ffc3f4d4eb96bcd815676f3c
Matiaja <102177224+Matiaja@users.noreply.github.com>
2023-05-29 11:03:55 -0300

    Adivinar el numero con ayuda
    Ejercicio 6 (Unidad 2 - capitulo 2)

---
+++ AdivineElNumeroConAyuda/Jugada.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdivineElNumero
+{
+    internal class Jugada
+    {
+        private int _adivino;
+        private int _intentos;
+        private int _numero;
+
+        public Jugada(int maxNumero)
+        {
+            Random rnd = new Random();
+            int Numero = rnd.Next(maxNumero);
+        }
+
+        public int Adivino
+        {
+            get => _adivino;
+            set => _adivino = value;
+        }
+
+        public int Intentos
+        {
+            get => _intentos;
+            set => _intentos = value;
+        }
+
+        public int Numero
+        {
+            get => _numero;
+            set => _numero = value;
+        }
+
+        public int Comparar(int numParaAdivinar, int numIngresado)
+        {
+            int distancia = Math.Abs(numParaAdivinar - numIngresado);
+            return distancia;    
+        }
+    }
+}
---
+++ AdivineElNumeroConAyuda/JugadaConAyuda.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdivineElNumero
+{
+    internal class JugadaConAyuda : Jugada
+    {
+        public JugadaConAyuda(int maxNumero) : base(maxNumero)
+        {
+        }
+
+            public void CompararConAyuda(int numParaAdivinar, int numIngresado)
+            {
+            if (base.Comparar(numParaAdivinar, numIngresado) <= 5 && base.Comparar(numParaAdivinar, numIngresado) > 0)
+            {
+                Console.WriteLine("Ya casi lo adivinas, estas muy cerca");
+            }
+            else if (base.Comparar(numParaAdivinar, numIngresado) <= 20 && base.Comparar(numParaAdivinar, numIngresado) > 5)
+            {
+
+                Console.WriteLine("No estas lejos, sigue intentando!!");
+            
+            }
+
+            else if (base.Comparar(numParaAdivinar, numIngresado) <= 100 && base.Comparar(numParaAdivinar, numIngresado) > 20)
+            {
+
+                Console.WriteLine("Estas demasiado lejos");
+
+            }
+
+
+            else if (base.Comparar(numParaAdivinar, numIngresado) <= int.MaxValue && base.Comparar(numParaAdivinar, numIngresado) > 100)
+            {
+                Console.WriteLine("Estas muy lejos");
+            }
+
+
+            }
+            
+
+            
+
+    }
+
+}
+
